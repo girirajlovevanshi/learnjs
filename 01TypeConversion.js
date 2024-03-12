8d6754c8fc4356d8232e1eae9d9d0632e81c458a
giri <girirajlovewanshi@gmail.com>
2024-03-13 00:57:32 +0530

    first commit

---
+++ 01TypeConversion.js
@@ -0,0 +1,11 @@
+// var, let, const
+
+const userId = 111;
+let username = "John";
+let isUserLoggedIn = true;
+
+console.log(userId)
+console.log(username)
+console.log(isUserLoggedIn)
+console.log(userId,username,isUserLoggedIn)
+console.table([userId,username,isUserLoggedIn])
