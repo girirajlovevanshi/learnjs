522db8dd68828edf036ead09997c2d4bbfaa571c
girirajlovevanshi <girirajlovewanshi@gmail.com>
2024-09-23 19:13:51 +0530

    getting error

---
+++ que/err001.js
@@ -0,0 +1,27 @@
+// //let  dogapi = 'https://dog.ceo/api/breeds/image/random'
+// let jokesApi = 'https://v2.jokeapi.dev/joke/Any'
+// let h2 = document.querySelector('h2');
+
+// function joke(){
+//     return new Promise((reslove, reject)=>{
+//         setTimeout(() => {
+//             fetch(jokesApi)
+//             .then((res)=>{
+//                 res.json()
+//                 .then((res)=>{
+//                     console.log(res);
+                    
+//                 })
+//             })
+//             .catch((err)=>{
+//                 console.log(err);
+//             })
+//         reslove();
+//         }, 10000);
+//     })
+// };
+// async function jokes() {
+//     await joke();
+//     console.log("DOne");
+// }
+// jokes();
